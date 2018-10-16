bdae50c478f479c53abe24afc8119a4a69c8db13
bowmanjeffs <jsbowman@ucsd.edu>
2018-10-15 21:43:13 -0700

    Added demo FCS files and cleaned things up a bit

--- fcm_model_AF.r
+++ fcm_model_AF.r
@@ -1,8 +1,8 @@
-setwd('~/bowman_lab/cyflow_space/ecuador_2018')
+setwd('~/bowman_lab/bowman_lab_github/flow_cytometry_scripts')
 
 #### general parameters ####
 
-output <- 'ecuador_2018_AF'
+output <- 'testing' # This variable will be used for naming your output files
 
 plot.fcm <- function(name, fcm.dataframe, beads, x, y){
   fcm.hex <- hexbin(log10(fcm.dataframe[,x]), log10(fcm.dataframe[,y]), 100)
@@ -23,14 +23,15 @@ plot.fcm <- function(name, fcm.dataframe, beads, x, y){
 
 #### QC parameters ####
 
-f.list <- list.files(path = '.', pattern = '*AF.fcs', ignore.case = T)
+f.list <- list.files(path = './', pattern = '*AF.fcs', ignore.case = T)
 FSC.limit <- 5
 SSC.limit <- 1
 FL6.limit <- 1
 
 #### aggregation and QC ####
 
-library(gplots)
+## The training.events dataframe holds a random sample
+## of each fcs file for training the neural network
 
 training.events <- data.frame(FSC = numeric(),
                                  SSC = numeric(),
@@ -40,8 +41,9 @@ training.events <- data.frame(FSC = numeric(),
                                  FL6 = numeric())
 
 library('flowCore')
+library('hexbin')
 
-#f.name <- f.list[7]
+#f.name <- f.list[1]
 #grep('blank', f.list)
 
 sample.size <- 1000 # size to sample from each for training data
@@ -79,8 +81,6 @@ for(f.name in f.list){
   plot.fcm(f.name, fcm, fcm.beads, 'SSC', 'FL5')
   plot.fcm(f.name, fcm, fcm.beads, 'SSC', 'FL6')
   
-  plot.fcm(f.name, fcm, fcm.beads, 'FL3', 'FL6')
-  
   ## Remove events that are below limits (thresholds).
   
   fcm <- fcm[fcm$FSC > FSC.limit,]
@@ -96,8 +96,6 @@ for(f.name in f.list){
   plot.fcm(paste(f.name, 'QC'), fcm, fcm.beads, 'SSC', 'FL4')
   plot.fcm(paste(f.name, 'QC'), fcm, fcm.beads, 'SSC', 'FL5')
   plot.fcm(paste(f.name, 'QC'), fcm, fcm.beads, 'SSC', 'FL6')
-  
-  plot.fcm(f.name, fcm, fcm.beads, 'FL3', 'FL6')
   
   ## blanks and other very clean samples may not have enough points to donate
   ## to training dataset
@@ -122,11 +120,17 @@ library(gplots)
 train.fcm <- function(event.file, params){
 
   events <- read.csv(event.file, stringsAsFactors = F)
+  
+  train.beads <- data.frame()
+  train.beads <- events[which(log10(events$SSC) > 3.5 &
+                                log10(events$FSC) > 2.5 &
+                                log10(events$FL6) > 2.5 &
+                                log10(events$FL3) > 2),]
 
-  plot.fcm('training', events, 'SSC', 'FSC')
-  plot.fcm('training', events, 'SSC', 'FL6')
-  plot.fcm('training', events, 'FL2', 'FL6')
-  plot.fcm('training', events, 'FL4', 'FL6')
+  plot.fcm('training', events, train.beads, 'SSC', 'FSC')
+  plot.fcm('training', events, train.beads, 'SSC', 'FL6')
+  plot.fcm('training', events, train.beads, 'FL2', 'FL6')
+  plot.fcm('training', events, train.beads, 'FL4', 'FL6')
 
   sample.mat <- as.matrix(events[,params])
   colnames(sample.mat) <- params
@@ -182,7 +186,7 @@ library(pmclust)
 
 ## K-mean clustering currently providing much more sensible clusters
 
-k <- 10
+k <- 5
 #som.cluster.sg <- kmeans(som.events.sg, centers = k.sg)$cluster
 som.cluster <- pmclust(som.events, K = k)$class
 #som.dist <- vegdist(som.events)
